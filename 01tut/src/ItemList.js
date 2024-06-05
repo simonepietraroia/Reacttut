10792ecd51447204ca51cf07e4d519699c97a39d
simonepietraroia <simonepietraroia21@gmail.com>
2024-06-05 11:20:12 +0000

    add item function adding

--- 01tut/src/ItemList.js
+++ 01tut/src/ItemList.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {FaTrashAlt} from 'react-icons/fa';
+import LineItem from './LineItem'
 
 const ItemList = ({items, handleCheck, handleDelete}) => {
   return (
     <ul>
           {items.map((item) => (
-            
+            <LineItem item = {item} handleCheck={handleCheck} handleDelete={handleDelete} key ={item.id}/>
           ))}
     </ul>
   )
--- 01tut/src/LineItem.js
+++ 01tut/src/LineItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {FaTrashAlt} from 'react-icons/fa';
 
-const LineItem = () => {
+const LineItem = ({item, handleCheck, handleDelete}) => {
   return (
-    <li className = 'item' key={item.id}>
+    <li className = 'item'>
         <input type='checkbox' checked={item.checked} onChange={() => handleCheck(item.id)}></input>
         <label onDoubleClick={() => handleCheck(item.id)} style={(item.checked) ? { textDecoration: 'line-through' } : null} >{item.item}</label>
-        <FaTrashAlt role='button' tabIndex='0' onClick={() => handleDelete(item.id)}/>
+        <FaTrashAlt role='button' tabIndex='0' onClick={() => handleDelete(item.id)} aria-label={`Delete ${item.item}`}/>
     </li>
   )
 }
