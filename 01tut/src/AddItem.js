71979615eefb9a1fc861a6892c2b9b6434a6f530
simonepietraroia <simonepietraroia21@gmail.com>
2024-06-09 14:32:22 +0000

    finishing part 9

--- 01tut/src/AddItem.js
+++ 01tut/src/AddItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
 
-const AddItem = () => {
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
   return (
-    <form className='addForm'> 
+    <form className='addForm' onSubmit={handleSubmit}> 
         <label htmlFor="addItem">Add Item</label>
-        <input autoFocus id='addItem' type='text' placeholder='Add Item' required/>
-        <button type='submit' aria-label='Add Item'></button>
+        <input autoFocus id='addItem' type='text' placeholder='Add Item' required value={newItem} onChange={(e) => setNewItem(e.target.value)}/>
+        <button type='submit' aria-label='Add Item'><FaPlus/></button>
     </form>
   )
 }
