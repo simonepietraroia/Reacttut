2078afda920d7683278152d709e2b072e574512f
simonepietraroia <simonepietraroia21@gmail.com>
2024-06-11 17:44:45 +0000

    finished checklist

--- 01tut/src/SearchItem.js
+++ 01tut/src/SearchItem.js
@@ -4,7 +4,7 @@ const SearchItem = ({ search, setSearch }) => {
   return (
     <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="search">Search</label>
-        <input type="text" id='search' role='searchbox' placeholder='Search Items' value={setSearch} onChange={(e) => setSearch(e.target.value)} />
+        <input type="text" id='search' role='searchbox' placeholder='Search Items' value={search} onChange={(e) => setSearch(e.target.value)} />
     </form>
   )
 }
